5c73309be331aac661357c1d57a2b018d1002243
songzhimin <songzhimin@qq.com>
2019-05-22 16:53:58 +0800

    测试一下增加和删除代码功能

--- pinyougou-common/src/main/java/com/pinyougou/util/CookieUtils.java
+++ pinyougou-common/src/main/java/com/pinyougou/util/CookieUtils.java
@@ -2,6 +2,7 @@ package com.pinyougou.util;
 
 public class CookieUtils {
     public static void main(String[] args) {
-        System.out.println("这里修改了aa");
+        System.out.println("删除了上面一条");
+        System.out.println("新增了这两条");
     }
 }
